406b7aca36fbabd3e9fbb243b05f81a914a84bc1
Paulo Barros <pbsfcin@gmail.com>
2016-01-05 15:25:01 -0300

    Adapts experiments script to work on linux systems.

--- experiments/experiments.sh
+++ experiments/experiments.sh
@@ -7,10 +7,16 @@ RESULTS=experiments/results.txt
 NUMBER_OF_ITER=10
 
 function index_test {
-    echo "Indexing file $1 $NUMBER_OF_ITER times took on average, in miliseconds:" >> $RESULTS
-    START=`gdate +%s%N`
-    for ((i=0; i<NUMBER_OF_ITER; i++)); do bin/ipmt index $1; done
-    END=`gdate +%s%N`
+    echo "Indexing and compressing file $1 $NUMBER_OF_ITER times took on average, in miliseconds:" >> $RESULTS
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        START=`date +%s%N`
+        for ((i=0; i<NUMBER_OF_ITER; i++)); do bin/ipmt index $1; done
+        END=`date +%s%N`
+    else
+        START=`gdate +%s%N`
+        for ((i=0; i<NUMBER_OF_ITER; i++)); do bin/ipmt index $1; done
+        END=`gdate +%s%N`
+    fi
     TOTAL_TIME=$(((END-START)/1000000))
     TOTAL_TIME=$((TOTAL_TIME/NUMBER_OF_ITER))
     echo $TOTAL_TIME >> $RESULTS
@@ -20,9 +26,17 @@ function index_test {
 
 function search_test {
     echo "Searching for the string $1 in file $2 $NUMBER_OF_ITER times took on average, in miliseconds:" >> $RESULTS
-    START=`gdate +%s%N`
-    for ((i=0; i<NUMBER_OF_ITER; i++)); do bin/ipmt search -c $1 $2; done
-    END=`gdate +%s%N`
+
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        START=`date +%s%N`
+        for ((i=0; i<NUMBER_OF_ITER; i++)); do bin/ipmt search -c $1 $2; done
+        END=`date +%s%N`
+    else
+        START=`gdate +%s%N`
+        for ((i=0; i<NUMBER_OF_ITER; i++)); do bin/ipmt search -c $1 $2; done
+        END=`gdate +%s%N`
+    fi
+
     TOTAL_TIME=$(((END-START)/1000000))
     TOTAL_TIME=$((TOTAL_TIME/NUMBER_OF_ITER))
     echo $TOTAL_TIME >> $RESULTS
