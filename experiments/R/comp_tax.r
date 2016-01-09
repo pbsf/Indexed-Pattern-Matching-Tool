166be3d5477bdf2a89101001819c18bebe1fe733
Paulo Barros <pbsfcin@gmail.com>
2016-01-09 16:24:12 -0300

    Moves comp_tax legend to bottom right.

--- experiments/R/comp_tax.r
+++ experiments/R/comp_tax.r
@@ -1,4 +1,4 @@
 mydata <- read.csv("data/comp_tax.csv", sep=" ")
 pdf("pdf/comp_tax.pdf", height=4.5, family="Times")
 matplot(mydata[, 1], mydata[, -1], type="l", xlab="Original file size in kbytes", ylab="Compression tax")
-legend("topright", legend = colnames(mydata)[-1], fill=seq_along(colnames(mydata)[-1]))
+legend("bottomright", legend = colnames(mydata)[-1], fill=seq_along(colnames(mydata)[-1]))
