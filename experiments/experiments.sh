fc4dc907f8aab307fff6fec907c91735b49c11ff
Paulo Barros <pbsfcin@gmail.com>
2016-01-09 11:43:37 -0300

    Updates experiment script.

--- experiments/experiments.sh
+++ experiments/experiments.sh
@@ -6,34 +6,35 @@ set -e
 RESULTS=experiments/results.txt
 NUMBER_OF_ITER=10
 
-function index_test {
+function index {
     echo "Indexing and compressing file $1 $NUMBER_OF_ITER times took on average, in miliseconds:" >> $RESULTS
-    if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        START=`date +%s%N`
-        for ((i=0; i<NUMBER_OF_ITER; i++)); do bin/ipmt index $1; done
-        END=`date +%s%N`
-    else
-        START=`gdate +%s%N`
-        for ((i=0; i<NUMBER_OF_ITER; i++)); do bin/ipmt index $1; done
-        END=`gdate +%s%N`
-    fi
-    TOTAL_TIME=$(((END-START)/1000000))
-    TOTAL_TIME=$((TOTAL_TIME/NUMBER_OF_ITER))
-    echo $TOTAL_TIME >> $RESULTS
-    echo "" >> $RESULTS
+    aux "bin/ipmt index " $1
 }
 
 
-function search_test {
+function search {
     echo "Searching for the string $1 in file $2 $NUMBER_OF_ITER times took on average, in miliseconds:" >> $RESULTS
+    aux "bin/ipmt search -c " $1 $2
+}
 
+function compress {
+    echo "Compressing file $1 $NUMBER_OF_ITER times took on average, in miliseconds:" >> $RESULTS
+    aux "bin/ipmt compress " $1
+}
+
+function decompress {
+    echo "Decompressing file $1 $NUMBER_OF_ITER times took on average, in miliseconds:" >> $RESULTS
+    aux "bin/ipmt decompress " $1
+}
+
+function aux {
     if [[ "$OSTYPE" == "linux-gnu" ]]; then
         START=`date +%s%N`
-        for ((i=0; i<NUMBER_OF_ITER; i++)); do bin/ipmt search -c $1 $2; done
+        for ((i=0; i<NUMBER_OF_ITER; i++)); do $3 $1 $2; done
         END=`date +%s%N`
     else
         START=`gdate +%s%N`
-        for ((i=0; i<NUMBER_OF_ITER; i++)); do bin/ipmt search -c $1 $2; done
+        for ((i=0; i<NUMBER_OF_ITER; i++)); do $3 $1 $2; done
         END=`gdate +%s%N`
     fi
 
@@ -48,6 +49,6 @@ cd ../
 rm -f $RESULTS
 make
 
-index_test big.txt
-search_test herself big.idx
+index big.txt
+search herself big.idx
 
